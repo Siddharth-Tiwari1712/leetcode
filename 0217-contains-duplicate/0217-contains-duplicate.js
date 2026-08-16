b7c8993aacbee35d4dd1cfe9174e5fba4f59bf6d
Siddharth Tiwari <62620333+Siddharth-Tiwari1712@users.noreply.github.com>
2026-08-16 16:14:40 +0530

    Time: 23 ms (61.44%), Space: 79 MB (60.71%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,16 @@
+function containsDuplicate(nums) {
+  return new Set(nums).size !== nums.length;
+}
+
+// Brute Force
+// function containsDuplicate(nums) {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] === nums[j]) {
+//         return true;
+//       }
+//     }
+//   }
+
+//   return false;
+// }
