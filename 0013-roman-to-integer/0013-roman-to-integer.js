0933e6994ae063c55dc90dfb791d56f3c7172ca5
Siddharth Tiwari <62620333+Siddharth-Tiwari1712@users.noreply.github.com>
2026-08-16 16:32:50 +0530

    Time: 3 ms (90.53%), Space: 59.9 MB (96.25%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,23 @@
+function romanToInt(s) {
+  const values = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  };
+
+  let total = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (i + 1 < s.length && values[s[i]] < values[s[i + 1]]) {
+      total -= values[s[i]];
+    } else {
+      total += values[s[i]];
+    }
+  }
+
+  return total;
+}
