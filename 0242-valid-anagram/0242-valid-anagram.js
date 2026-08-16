8481e8fde6102d93debe5b68d885987332147e11
Siddharth Tiwari <62620333+Siddharth-Tiwari1712@users.noreply.github.com>
2026-08-16 16:14:22 +0530

    Time: 14 ms (58.18%), Space: 60 MB (20.88%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,32 @@
+function isAnagram(s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+
+  const count = {};
+
+  for (let char of s) {
+    count[char] = (count[char] || 0) + 1;
+  }
+
+  for (let char of t) {
+    if (!count[char]) {
+      return false;
+    }
+
+    count[char]--;
+  }
+
+  return true;
+}
+
+
+// Sorting is O(n logn)
+// function isAnagram(s, t) {
+//   if (s.length !== t.length) {
+//     return false;
+//   }
+
+//   return s.split('').sort().join('') ===
+//          t.split('').sort().join('');
+// }
